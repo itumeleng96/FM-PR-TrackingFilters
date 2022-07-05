75a2a55b3dc98144a7bf325bd507e4a385fcd9b0
itumeleng <imalemela@csir.co.za>
2022-07-05 21:37:34 +0200

    Updated Kmeans algorithm

--- kMeans.m
+++ kMeans.m
@@ -1,53 +1,38 @@
-function [ cluster, centr ] = kMeans( k, P )
+function [ cluster, centroids ] = kMeans( k, dataset )
 
+numberOfPoints = size(dataset,2); 
+dimensionOfPoints = size(dataset,1); 
 
-numP = size(P,2); % number of points
-dimP = size(P,1); % dimension of points
+randomIndices = randperm(numberOfPoints,k);
+centroids = dataset(:,randomIndices);
 
+cluster = zeros(1,numberOfPoints);
 
-%% Choose k data points as initial centroids
-% choose k unique random indices between 1 and size(P,2) (number of points)
-randIdx = randperm(numP,k);
-% initial centroids
-centr = P(:,randIdx);
-
-
-%% Repeat until stopping criterion is met
-
-% init cluster array
-cluster = zeros(1,numP);
-
-% init previous cluster array clusterPrev (for stopping criterion)
 clusterPrev = cluster;
 
-% for reference: count the iterations
 iterations = 0;
 
-% init stopping criterion
-stop = false; % if stopping criterion met, it changes to true
+stop = false;
 
-while stop == false
-    
+while stop == false    
     % for each data point 
-    for idxP = 1:numP
-        % init distance array dist
+    for indexPoint = 1:numberOfPoints
+        
         dist = zeros(1,k);
         % compute distance to each centroid
-        for idxC=1:k
-            dist(idxC) = norm(P(:,idxP)-centr(:,idxC));
+        for indexCluster=1:k
+            dist(indexCluster) = norm(dataset(:,indexPoint)-centroids(:,indexCluster));
         end
         % find index of closest centroid (= find the cluster)
         [~, clusterP] = min(dist);
-        cluster(idxP) = clusterP;
+        cluster(indexPoint) = clusterP;
     end
     
     % Recompute centroids using current cluster memberships:
     
-    centr = zeros(dimP,k);
-    % for every cluster compute new centroid
-    for idxC = 1:k
-        % find the points in cluster number idxC and compute row-wise mean
-        centr(:,idxC) = mean(P(:,cluster==idxC),2);
+    centroids = zeros(dimensionOfPoints,k);
+    for indexC = 1:k
+        centroids(:,indexC) = mean(dataset(:,cluster==indexC),2);
     end
     
     if clusterPrev==cluster
@@ -55,9 +40,7 @@ while stop == false
     end
     
     clusterPrev = cluster;
-    
     iterations = iterations + 1;
     
 end
-
 end

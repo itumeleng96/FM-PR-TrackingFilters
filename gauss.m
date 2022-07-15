301752fcd7f920fbf0a4ca50dac07c93d08467fb
itumeleng <imalemela@csir.co.za>
2022-07-15 16:05:18 +0200

    moved files

--- gauss.m
+++
@@ -1,6 +0,0 @@
-sig = randn(1,5000000)+j*randn(1,5000000);
-I = real(sig);
-Q = imag(sig);
-fprintf('Writing HDF5 data...\n');
-hdf5write('waveform.h5', '/I/value', real(I), '/Q/value', imag(Q));
-fprintf('Complete.\n');

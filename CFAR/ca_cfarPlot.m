00c2480ba3f2f61ccea4cfccd472a6338036ca9d
imalemela <imalemela@csir.co.za>
2022-11-30 17:58:54 +0200

    Code refactoring

---
+++ CFAR/ca_cfarPlot.m
@@ -0,0 +1,71 @@
+%Function to do CA CFAR
+%pf: Probability of False Alarm 
+%N : Number of samples arounf the Cell Under Test
+%G : Number of Guard cells
+%cut : the magnitiude cells of the Range Doppler map
+
+function targetClusters = ca_cfarPlot(RDM,rate_fa,fs,fd_max,td_max,index,f)
+%Generates a CFAR output map in the range-doppler domain
+%Firstly calculate the interference power from the average of N samples in
+%the vicinity of the CUT
+
+%Parameters to allow zoom in
+xlim_upper = 2e-4;
+ylim_upper = 200;
+ylim_lower = -200;
+
+trc_num = 2;
+guac_num = 4;
+
+[rows, cols] = size(RDM);
+RDM_final = zeros(rows, cols); 
+
+
+for r=1:rows
+    for c=1:cols
+        % get guard cells sum value
+        [guard_value, g_c] = subarea(RDM, c, r, guac_num);
+        
+        % get training cells sum value
+        [train_value, t_c] = subarea(RDM, c, r, guac_num + trc_num);
+        
+        Num_train = t_c - g_c - 1;
+        train_value = (train_value - guard_value) / (Num_train); %Noise Power
+        train_value = pow2db(train_value);
+       
+        
+        alpha = Num_train*((1-rate_fa)^(-1/Num_train) - 1);  %threshold factor
+        threshold_val = train_value * alpha;
+        %threshold_val = train_value * offset;
+        
+        if RDM(r,c) >= threshold_val
+            RDM_final(r,c) = 1;
+        end
+    end
+end
+
+Ndelay = floor(td_max*fs);                                  %number of points corresponding to td_max
+time = 0:1/fs:Ndelay/fs;
+frequency = -fd_max:1:fd_max;
+
+figure(f);
+imagesc(time,frequency,RDM_final);
+text(0,10,"Time:" + index+ "s");
+axis xy;
+colorbar;
+xlabel('Bistatic delay [s]','Fontsize',10);
+ylabel('Doppler frequency [Hz]','Fontsize',10);
+grid on;
+title('CFAR and Centroids');
+xlim([0 xlim_upper]) 
+ylim([ylim_lower ylim_upper])
+hold on;
+
+RDM = RDM_final;
+
+[row,column] = find(RDM>0);
+row= row.';
+column = column.';
+
+targetClusters = [column; row];
+end

57586f8733db7091ce058d0bc0ca2f2ff7ae5839
itumeleng <imalemela@csir.co.za>
2024-02-22 17:42:05 +0200

    working HCSKF filter

--- TrackingFilter-KalmanFilter/kalmanFilter.m
+++ TrackingFilter-KalmanFilter/kalmanFilter.m
@@ -1,7 +1,7 @@
 classdef kalmanFilter
 
     properties
-        dt,U,X,F,A,H,Q,R,P,S,coeff,measured_x,measured_y,std_acc,wk,count,updater,update1;
+        dt,U,X,F,A,H,Q,R,P,S,coeff,measured_x,measured_y,std_acc,wk,epsDoppler;
     end
     
     methods
@@ -40,7 +40,7 @@ classdef kalmanFilter
             
             obj.P = [500,0,0,0;                              % Initial Error Covariance Matrix
                      0, 10, 0, 0;
-                     0, 0, 0.4,0;
+                     0, 0, 1,0;
                      0, 0, 0, 0.1];  
             
             obj.A = [1, dt, 0, 0;
@@ -49,10 +49,10 @@ classdef kalmanFilter
                      0, 0, 0, 1;];
 
             obj.wk = std_acc*[dt^2;dt;dt^2;dt];
+            obj.S = obj.R;
 
-            obj.count =0;
-            obj.updater =0;
-            obj.update1 =0;
+            obj.epsDoppler =[];
+
 
         end
         
@@ -73,66 +73,102 @@ classdef kalmanFilter
         function [X_est,KF_obj2] = update(obj,z)
             %UPDATE STAGE
 
-            %ADAPTIVE FILTERING
-            %Threshold to compare with Likelihood
-            threshold=3;
-
-            %max number of adaptive filtering
-            max_adapt=4;
-
-            %%To be replaced with another scheme to detect steady state
-            obj.count = obj.count+1;    
-
-            %Use only doppler for abnomarl measurments checking
-            %S = H*P*H'+ R
-            obj.S = obj.H * obj.P * obj.H.' + obj.R;
-
-            eps_ =log(normpdf(z(2),obj.X(3,1),obj.S(2,2)));
-
-            if(abs(eps_)>threshold && obj.count>10 && obj.updater<max_adapt && obj.update1>max_adapt)
-
-                obj.updater = obj.updater+1;
-                if(obj.updater==max_adapt)
-                    obj.update1=0;
-                end
-
-                k_p = (obj.P * obj.H.') * obj.S^(-1);
-
-                r_adapt = obj.R;
-                r_adapt(2,2) =r_adapt(2,2)*1000;
-
-                obj.S = obj.H * obj.P * obj.H.' + r_adapt;
-
-                K = (obj.P * obj.H.') * obj.S^(-1);
-
-                obj.X = obj.X + K * (z-obj.H * obj.X);
-                I = eye(size(obj.H,2));
-        
-                %UPDATE ERROR COVARIANCE MATRIX
-                obj.P = (I - (k_p * obj.H)) * obj.P ;
-            else
-                obj.updater =0;
-                obj.update1=obj.update1+1;
-                obj.S = obj.H * obj.P * obj.H.' + obj.R;
-                
-                %K = PH'inv(S)
-                
-                K = (obj.P * obj.H.') * obj.S^(-1);
-                
-                obj.X = obj.X + K * (z-obj.H * obj.X);
-                I = eye(size(obj.H,2));
-        
-                %UPDATE ERROR COVARIANCE MATRIX
-                obj.P = (I - (K * obj.H)) * obj.P ;
-            end
- 
+            %Adaptive estimation of R matrix
            
 
-          
+            %S = H*P*H'+ R
+            obj.S = obj.H * obj.P * obj.H.' + obj.R;
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % Covariance Scaling Kalman Step 
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %Mk = e'.[HPH' +R]^−1.ek .
+            ek = z-obj.H*obj.X;
+            
+            ek2_doppler = ek(2)^2;
+            disp("ek^2");
+            disp(ek2_doppler);
+            disp(obj.S(2,2));
+            eps_doppler = ek2_doppler/obj.S(2,2);  
+
+            obj.epsDoppler = [obj.epsDoppler,eps_doppler];
+            
+            M=5;
+            if(size(obj.epsDoppler,2)>M)
+                average_eps = mean(obj.epsDoppler(end-M:end-1));
+                disp(average_eps);
+                disp(eps_doppler);
+                if(eps_doppler>2*average_eps && eps_doppler>1)
+                    disp("outlier");
+                    obj.R = abs([0;eps_doppler-1].*(obj.H*obj.P*obj.H') + [1;eps_doppler*500].*obj.R);
+                    obj.S = obj.H * obj.P * obj.H.' + obj.R;
+                    %remove outlier from average
+                    obj.epsDoppler =obj.epsDoppler(end-M:end-1);
+                else
+                    obj.R = [500,0;0,0.01];
+                    obj.S = obj.H * obj.P * obj.H.' + obj.R;
+                end
+            end
+
+
+            %K = PH'inv(S)
+            K = (obj.P * obj.H.') * obj.S^(-1);
+            
+            %Robust estimation
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %Represent Kalman filter as linear regression problem
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Calculate dimensions of P and R matrices
+            [num_rows_P, num_cols_P] = size(obj.P);
+            [num_rows_R, num_cols_R] = size(obj.R);
+            
+
+            % Create block matrix representing covariance of error'
+
+            epsilon_covariance_range =  [obj.P(1:2,1:2) ,zeros(num_rows_P/2, num_cols_R/2); 
+                                   zeros(num_rows_R/2, num_cols_P/2), obj.R(1,1)];
+
+            epsilon_covariance_doppler = [obj.P(3:4,3:4), zeros(num_rows_P/2, num_cols_R/2); 
+                                   zeros(num_rows_R/2, num_cols_P/2), obj.R(2,2)]; 
+            
+
+            Sadapt_range = chol(epsilon_covariance_range,'lower');
+            Sadapt_doppler = chol(epsilon_covariance_doppler,'lower');
+
+            Ydoppler = Sadapt_doppler\ [obj.X(3:4);z(2)];
+            Yrange = Sadapt_range \ [obj.X(1:2);z(1)];
+                        
+            Xadapt_range = Sadapt_range\ [eye(2);[1,0;]];
+            Xadapt_doppler = Sadapt_doppler\ [eye(2); [1,0;]];
+        
+            % Define the anonymous function for the criterion
+            robustScore = HuberScore(1.5);
+            robustScoreH = HuberScore(1.5);
+            
+            % Initial guess for the state vector
+            x0_range = obj.X(1:2);
+            x0_doppler = obj.X(3:4);
+            
+            [res_doppler, ~] = fminsearch(@(xx) m_estimate_criterion(xx, Ydoppler, Xadapt_doppler,robustScoreH), x0_doppler);
+
+            [res_range, ~] = fminsearch(@(xx) m_estimate_criterion(xx, Yrange, Xadapt_range,robustScore), x0_range);
+
+            
+            X_res = [res_range;res_doppler];
+            obj.X = X_res;
+
+            %obj.X = obj.X + K * (z-obj.H * obj.X);
+            
+            I = eye(size(obj.H,2));
+    
+            %UPDATE ERROR COVARIANCE MATRIX
+            obj.P = (I - (K * obj.H)) * obj.P ; 
+            obj.R = [500,0;0,0.1];
+            
             X_est = obj.X;
             KF_obj2 = obj;
         end
-
         
      end
  end

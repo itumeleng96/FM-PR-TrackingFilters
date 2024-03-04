db7fc330c293abc6521914dfb22c18cd1a41700f
itumeleng <imalemela@csir.co.za>
2024-03-04 17:10:40 +0200

    Cell average calculated using doppler dimension only

--- CFAR/subarea.m
+++ CFAR/subarea.m
@@ -1,16 +1,19 @@
-function [value, elem_c] = subarea(data, pos_x, pos_y, y_distance,x_distance)
-    % the function estimates and avg value for the specified submatrix
-    % using one dimension
+function [value, elem_c] = subarea(data, pos_x, pos_y, y_distance)    
+    % Multi-Deimonsional CFAR 
+    % function [value, elem_c] = subarea(data, pos_x, pos_y, y_distance,x_distance)
+    %
+    % The function estimates and avg value for the specified submatrix
+    % in 1-D for Doppler and Range Map (Range-resolution low for FM)
     
     % get min/max indices %commented to sum only in one dimension
     min_y = max([pos_y - y_distance, 1]); 
     max_y = min([size(data,1), pos_y + y_distance]);
    
-    min_x = max([pos_x - x_distance, 1]);
-    max_x = min([size(data,2), pos_x + x_distance]);
+    %min_x = max([pos_x - x_distance, 1]);
+    %max_x = min([size(data,2), pos_x + x_distance]);
 
-    submatrix = data(min_y:max_y, min_x:max_x);
-    %submatrix = data(min_y:max_y,pos_x);
+    %submatrix = data(min_y:max_y, min_x:max_x);
+    submatrix = data(min_y:max_y,pos_x);
     [r , c] = size(submatrix);
     
     newsubmatrix = submatrix; % convert to linear space

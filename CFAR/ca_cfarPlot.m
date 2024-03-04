9cd2ed1142ddf4f2dd07fefb51b8a1f1b89edfec
itumeleng <imalemela@csir.co.za>
2024-03-04 17:10:55 +0200

    Code for testing CFAR

--- CFAR/ca_cfarPlot.m
+++ CFAR/ca_cfarPlot.m
@@ -1,15 +1,18 @@
-%Function to do CA CFAR
+% Cell-Averaging Constant False Alarm Rate
+% Norman Pearson Detector
+% Author: Itumeleng Malemela
+
+function [targetClusters,RDM,rdm_] = ca_cfarPlot(RDM,pfa,fs,fd_max,td_max,index,f,rdm)
+%Generates a CFAR output map in the range-doppler domain
+
+%Firstly calculate the interference power from the average of N samples in
+%the vicinity of the CUT
+
 %pf: Probability of False Alarm 
 %N : Number of samples around the Cell Under Test
 %G : Number of Guard cells
 %cut : the magnitiude cells of the Range Doppler map
 
-function [targetClusters,RDM,rdm_] = ca_cfarPlot(RDM,pfa,fs,fd_max,td_max,index,f,rdm)
-%Generates a CFAR output map in the range-doppler domain
-%Firstly calculate the interference power from the average of N samples in
-%the vicinity of the CUT
-%Parameters to allow zoom in
-xlim_upper = 2e-4;
 ylim_upper = 200;
 ylim_lower = -200;
 
@@ -17,20 +20,24 @@ y_guard_num = 8;
 y_train_num = 10;
 
 
-x_guard_num = 8;
-x_train_num = 6;
+%x_guard_num = 8;
+%x_train_num = 6;
 
 [rows, cols] = size(RDM);
 RDM_final = zeros(rows, cols); 
+threshold_values = zeros(rows, cols);
 
 for r=1:rows
     for c=1:cols
         % get guard cells sum value
-        [guard_value, g_c] = subarea(RDM, c, r, y_guard_num,x_guard_num);
+
+        %[guard_value, g_c] = subarea(RDM, c, r, y_guard_num,x_guard_num) 
+        [guard_value, g_c] = subarea(RDM, c, r, y_guard_num);
         
-        % get training cells sum value
-        [train_value, t_c] = subarea(RDM, c, r, y_guard_num+y_train_num,x_guard_num+x_train_num);
-        
+        % Get training cells sum value
+        % [train_value, t_c] = subarea(RDM, c, r, y_guard_num+y_train_num,x_guard_num+x_train_num);
+        [train_value, t_c] = subarea(RDM, c, r, y_guard_num+y_train_num);
+
         Num_train = t_c - g_c - 1;
         train_value = (train_value - guard_value) / (Num_train);   %Noise Power
         %train_value = pow2db(train_value);                        %Noise Power in db
@@ -39,14 +46,13 @@ for r=1:rows
         alpha = Num_train*((pfa)^(-1/Num_train) - 1);              %threshold factor
         threshold_val = train_value * alpha;
         %threshold_val = train_value * offset;
+        threshold_values(r,c) = threshold_val;
         
         if RDM(r,c) >= threshold_val
 
-            %RDM_final(r,c) = 1;
-
             % Calculate SNR for the detected cell and 
             % plot SNR instead of 0 or 1
-        
+
             signal_power = RDM(r, c);
             noise_power = train_value;
             SNR = signal_power/noise_power;
@@ -58,26 +64,45 @@ for r=1:rows
 end
 
 c=3e8;
-Ndelay = floor(td_max*fs);                                  %number of points corresponding to td_max
+Ndelay = floor(td_max*fs);                                  
 time = 0:1/fs:Ndelay/fs;
 range = time*c;
 frequency = -fd_max:1:fd_max;
 
 RDM_final = pow2db(RDM_final);
-%if index==1
 rdm = RDM_final ;
-%end
-
-%if index>1
-%    rdm =rdm+RDM_final ;
-%end
-%rdm = min(rdm, 1);  % Set maximum value to 1
 
 rdm_ = rdm;
 max_db = max(max(rdm_));
 dyn_db = 40;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% NB this is just to validate that CFAR works
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Find the index of the range value closest to 1e4
+%{
+[~, range_idx] = min(abs(range - 1e4));
 
+% Extract the frequency levels for the specific range line
+frequency_levels = frequency;
+specific_range_line_rdm = pow2db(RDM(:, range_idx));
+specific_range_line_threshold = pow2db(threshold_values(:, range_idx));
+
+% Plot the 1D graph
+figure(10);
+clf;
+plot(frequency_levels, specific_range_line_rdm, 'LineWidth', 2);
+hold on;
+plot(frequency_levels, specific_range_line_threshold, 'LineWidth', 2);
+% Set the axis labels and title
+xlabel('Frequency');
+ylabel('Signal Levels(DB)');
+title('CFAR validation for Range:10KM');
+text(0, 10, "Time: " + index + "s", 'Color', 'white'); 
+%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Plot CA CFAR results with Signal to Noise ratio
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(f);
 imagesc(range, frequency, rdm_,[max_db-dyn_db max_db]);
 colormap(gca, 'hot');
@@ -86,16 +111,15 @@ colormap(gca, 'hot');
 c = colorbar;
 c.Label.String = 'SNR (dB)';
 c.FontSize = 10;
-c.Color = 'black'; % Set colorbar label color to white
+c.Color = 'black'; 
 
-set(gca, 'Color', 'black'); % Set background color to black
-text(0, 10, "Time: " + index + "s", 'Color', 'white'); % Set text color to white
+set(gca, 'Color', 'black'); 
+text(0, 10, "Time: " + index + "s", 'Color', 'white'); 
 axis xy;
-xlabel('Bistatic range [m]', 'FontSize', 10, 'Color', 'black'); % Set xlabel color to white
-ylabel('Bistatic Doppler frequency [Hz]', 'FontSize', 10, 'Color', 'black'); % Set ylabel color to white
+xlabel('Bistatic range [m]', 'FontSize', 10, 'Color', 'black');
+ylabel('Bistatic Doppler frequency [Hz]', 'FontSize', 10, 'Color', 'black');
 grid on;
-title('CFAR and Centroids', 'Color', 'black'); % Set title color to white
-%xlim([0 xlim_upper]);
+title('CFAR and Centroids', 'Color', 'black'); 
 ylim([ylim_lower ylim_upper]);
 
 RDM = RDM_final;
@@ -109,6 +133,5 @@ range_values = range(column.');         % convert time values to range values
 frequency_values = frequency(row.');    % get frequency values
 
 targetClusters = [range_values; frequency_values]; % store centroids as range and frequency
-% Get SNRs for indices in Target Clusters
 
 end

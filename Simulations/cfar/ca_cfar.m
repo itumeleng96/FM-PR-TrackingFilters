ce4b88409055c0a8aea4523dd9ef3c23c631dc20
itumeleng <imalemela@csir.co.za>
2024-09-27 12:56:49 +0200

    CFAR

--- Simulations/cfar/ca_cfar.m
+++ Simulations/cfar/ca_cfar.m
@@ -4,86 +4,68 @@
 %G : Number of Guard cells
 %cut : the magnitiude cells of the Range Doppler map
 
-function [targetClusters,RDM,rdm_] = ca_cfar(RDM,pfa,fs,fd_max,td_max,index,rdm)
-%Generates a CFAR output map in the range-doppler domain
-
-%Firstly calculate the interference power from the average of N samples in
-%the vicinity of the CUT
-
-%pf: Probability of False Alarm 
-%N : Number of samples around the Cell Under Test
-%G : Number of Guard cells
-%cut : the magnitiude cells of the Range Doppler map
-
-y_guard_num = 8;
-y_train_num = 10;
-
-
-%x_guard_num = 8;
-%x_train_num = 6;
-
-[rows, cols] = size(RDM);
-RDM_final = zeros(rows, cols); 
-threshold_values = zeros(rows, cols);
-
-for r=1:rows
-    for c=1:cols
-        % get guard cells sum value
-
-        %[guard_value, g_c] = subarea(RDM, c, r, y_guard_num,x_guard_num) 
-        [guard_value, g_c] = subarea(RDM, c, r, y_guard_num);
-        
-        % Get training cells sum value
-        % [train_value, t_c] = subarea(RDM, c, r, y_guard_num+y_train_num,x_guard_num+x_train_num);
-        [train_value, t_c] = subarea(RDM, c, r, y_guard_num+y_train_num);
-
-        Num_train = t_c - g_c - 1;
-        train_value = (train_value - guard_value) / (Num_train);   %Noise Power
-        %train_value = pow2db(train_value);                        %Noise Power in db
-       
-        
-        alpha = Num_train*((pfa)^(-1/Num_train) - 1);              %threshold factor
-        threshold_val = train_value * alpha;
-        %threshold_val = train_value * offset;
-        threshold_values(r,c) = threshold_val;
-        
-        if RDM(r,c) >= threshold_val
-
-            % Calculate SNR for the detected cell and 
-            % plot SNR instead of 0 or 1
-
-            signal_power = RDM(r, c);
-            noise_power = train_value;
-            SNR = signal_power/noise_power;
-            RDM_final(r,c) =SNR;
-          
-            
+function [targetClusters,RDM,rdm_] = ca_cfar(RDM,pfa,fs,fd_max,td_max,maxTresholdValue)
+    % Generates a CFAR output map in the range-doppler domain
+    
+        y_guard_num = 4;
+        y_train_num = 9;
+    
+        [rows, cols] = size(RDM);
+        RDM_final = zeros(rows, cols); 
+        RDM_final_SNR = zeros(rows, cols); 
+    
+        threshold_values = zeros(rows, cols);
+    
+        for r = 1:rows
+            for c = 1:cols
+                % Get guard cells sum value
+                [guard_value, g_c] = subarea(RDM, c, r, y_guard_num);
+                
+                % Get training cells sum value
+                [train_value, t_c] = subarea(RDM, c, r, y_guard_num + y_train_num);
+    
+                Num_train = t_c - g_c - 1;
+                train_value = (train_value - guard_value) / Num_train;   % Noise Power
+                
+                alpha = Num_train * ((pfa)^(-1 / Num_train) - 1);        % Threshold factor
+                threshold_val = train_value * alpha;
+                threshold_values(r, c) = threshold_val;
+                
+                if RDM(r, c) >= threshold_val
+                    signal_power = RDM(r, c);
+                    noise_power = train_value;
+                    SNR = signal_power/noise_power;
+                    RDM_final_SNR(r,c) =SNR;
+    
+                    RDM_final(r, c) = 1;  % Detected target
+                else
+                    RDM_final(r, c) = 0;  % No target
+                end
+            end
         end
+    
+        c = 3e8;
+        Ndelay = floor(td_max * fs);                                  
+        time = 0:1/fs:Ndelay/fs;
+        range = time * c;
+        range = range / 1000;
+        frequency = -fd_max:1:fd_max;
+    
+        
+        RDM_final_snr = pow2db(RDM_final_SNR);
+        rdm_ = RDM_final_snr;
+
+        max_db = max(max(RDM_final_snr));
+    
+        centroid_threshold = max_db -maxTresholdValue; %Set Centroid to 20db less
+        [row, column] = find(RDM_final_snr> centroid_threshold);
+    
+        % Get target clusters as Bistatic Range and Doppler values
+    
+        range_values = range(column.');         % convert time values to range values
+        frequency_values = frequency(row.');    % get frequency values
+    
+        targetClusters = [range_values; frequency_values]; % store centroids as range and frequency
+    
+    
     end
-end
-
-c=3e8;
-Ndelay = floor(td_max*fs);                                  
-time = 0:1/fs:Ndelay/fs;
-range = time*c;
-frequency = -fd_max:1:fd_max;
-
-RDM_final = pow2db(RDM_final);
-rdm = RDM_final ;
-
-rdm_ = rdm;
-max_db = max(max(rdm_));
-
-RDM = RDM_final;
-
-centroid_threshold = max_db -10; %Set Centroid to 20db less
-[row, column] = find(RDM > centroid_threshold);
-
-% Get target clusters as Bistatic Range and Doppler values
-
-range_values = range(column.');         % convert time values to range values
-frequency_values = frequency(row.');    % get frequency values
-
-targetClusters = [range_values; frequency_values]; % store centroids as range and frequency
-
-end
--- Simulations/cfar/ca_cfarPlot.m
+++ Simulations/cfar/ca_cfarPlot.m
@@ -16,8 +16,8 @@ function [targetClusters,RDM,rdm_] = ca_cfarPlot(RDM,pfa,fs,fd_max,td_max,index,
 ylim_upper = 200;
 ylim_lower = -200;
 
-y_guard_num = 8;
-y_train_num = 10;
+y_guard_num = 2;
+y_train_num = 5;
 
 
 %x_guard_num = 8;
@@ -67,6 +67,7 @@ c=3e8;
 Ndelay = floor(td_max*fs);                                  
 time = 0:1/fs:Ndelay/fs;
 range = time*c;
+range = range/1000;
 frequency = -fd_max:1:fd_max;
 
 RDM_final = pow2db(RDM_final);
@@ -75,13 +76,12 @@ rdm = RDM_final ;
 rdm_ = rdm;
 max_db = max(max(rdm_));
 dyn_db = 40;
-
+%{
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% NB this is just to validate that CFAR works
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Find the index of the range value closest to 1e4
-%{
-[~, range_idx] = min(abs(range - 1e4));
+[~, range_idx] = min(abs(range - 15));
 
 % Extract the frequency levels for the specific range line
 frequency_levels = frequency;
@@ -91,20 +91,23 @@ specific_range_line_threshold = pow2db(threshold_values(:, range_idx));
 % Plot the 1D graph
 figure(10);
 clf;
-plot(frequency_levels, specific_range_line_rdm, 'LineWidth', 2);
+plot(frequency_levels, specific_range_line_rdm, 'LineWidth', 2, 'DisplayName', 'Signal Level');
 hold on;
-plot(frequency_levels, specific_range_line_threshold, 'LineWidth', 2);
+plot(frequency_levels, specific_range_line_threshold, 'LineWidth', 2, 'DisplayName', 'Threshold');
 % Set the axis labels and title
-xlabel('Frequency');
-ylabel('Signal Levels(DB)');
-title('CFAR validation for Range:10KM');
-text(0, 10, "Time: " + index + "s", 'Color', 'white'); 
+xlabel('Bistatic Doppler Frequency');
+ylabel('Signal Levels (dB)');
+title('CFAR Validation for Bistatic Range: 15 km');
+text(0, 10, "Time: " + index + "s", 'Color', 'white');
+
+% Add legend
+legend('show');
 %}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Plot CA CFAR results with Signal to Noise ratio
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(f);
-imagesc(range, frequency, rdm_,[max_db-dyn_db max_db]);
+imagesc(time, frequency, rdm_,[max_db-dyn_db max_db]);
 colormap(gca, 'hot');
 %colormap(gca, 'gray');
 
@@ -114,17 +117,17 @@ c.FontSize = 10;
 c.Color = 'black'; 
 
 set(gca, 'Color', 'black'); 
-text(0, 10, "Time: " + index + "s", 'Color', 'white'); 
+%text(0, 10, "Time: " + index + "s", 'Color', 'white'); 
 axis xy;
-xlabel('Bistatic range [m]', 'FontSize', 10, 'Color', 'black');
+xlabel('Bistatic Delay [s]', 'FontSize', 10, 'Color', 'black');
 ylabel('Bistatic Doppler frequency [Hz]', 'FontSize', 10, 'Color', 'black');
 grid on;
-title('CFAR and Centroids', 'Color', 'black'); 
+title('CA CFAR', 'Color', 'black'); 
 ylim([ylim_lower ylim_upper]);
 
 RDM = RDM_final;
 
-centroid_threshold = max_db -10; %Set Centroid to 20db less
+centroid_threshold = max_db -40; %Set Centroid to 20db less
 [row, column] = find(RDM > centroid_threshold);
 
 % Get target clusters as Bistatic Range and Doppler values

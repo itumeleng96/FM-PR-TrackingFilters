420c411345033c8f94052897ab07c2c7943f5a9c
itumeleng <imalemela@csir.co.za>
2024-09-27 13:48:09 +0200

    tuning kalman filter

--- Simulations/TuningFilters/runTuneKalmanFilter.m
+++ Simulations/TuningFilters/runTuneKalmanFilter.m
@@ -43,7 +43,7 @@ dopplerMeasurementData = h5read('./measurement_data.h5', '/doppler_shifts');
 
 
 
-figure(f1);
+figure(f);
 hold on; 
 plot(rangeTrueData, dopplerTrueData, 'b', 'DisplayName', 'Ground Truth');
 plot(rangeMeasurementData, dopplerMeasurementData, 'r--', 'DisplayName', 'Measurement Data');
@@ -55,12 +55,36 @@ ylabel('Bistatic Doppler Shift (Hz)');
 xlim([0 100]);
 ylim([-200 200]);
 
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Bayesian Optimization  On the Kalman Filter
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%Kalman filter
+std_meas=[4,0.2];                           
+std_acc=[0.001,0.02]; 
+x_initial =[rangeMeasurementData(1),dopplerMeasurementData(1)];
+dt = 1;                           
+KF_object = kalmanFilter(dt,std_acc,std_meas(1),std_meas(2),[x_initial(1);0;x_initial(2);0;]);
+filterPredictionsDoppler = [];
+filterPredictionsRange = [];
+
+for i = 1:59
+        %Prediction
+        [X_pred,KF_object1] = KF_object.predict();
+        filterPredictionsRange(end+1)= X_pred(1);
+        filterPredictionsDoppler(end+1) = X_pred(3);
+
+        %Update stage
+        [X_est,KF_object2] = KF_object1.update([rangeMeasurementData(i);dopplerMeasurementData(i);]);
+
+        KF_object = KF_object2;
+end
+
+plot(filterPredictionsRange, filterPredictionsDoppler, '^-', 'DisplayName', ' Predictions');
+
 legend('show');
 hold off;
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Bayesian Optimization 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
